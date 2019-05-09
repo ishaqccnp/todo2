bbf0b86b57c93678d2051cb4ec84cbe98deba156
Mohammad Ishaq <mobile.ishaq1@gmail.com>
2019-05-09 13:43:47 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import uuid from "uuid";
+import "bootstrap/dist/css/bootstrap.css";
+class App extends Component {
+  state = {
+    todos: [{ id: uuid.v4(), title: "book tickets", editItem: true }]
+  };
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-center ">Todo App</h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
