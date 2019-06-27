cf7550dbd9759746a02816e5f73c194600d60bae
Mohammad Ishaq <mobile.ishaq1@gmail.com>
2019-06-27 14:31:09 -0500

    Update index.js
    changed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+//import
+const Test = () => {
+  return <h1>test</h1>;
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default Test;
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
